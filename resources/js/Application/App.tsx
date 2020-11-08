3d433b602d5fd5083304f5e488e461d7da7e5f5a
Vitaly <vita-dubov@yandex.ru>
2020-11-08 16:46:43 +0400

    init

---
+++ resources/js/Application/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavApp from "./components/Main/NavApp";
+import Routes from "./components/Routes";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <NavApp />
+    </>
+  );
+};
+
+export default App;
+
---
+++ resources/js/Application/components/Routes.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import ItemsList from "./Main/ItemsList";
+import ProductsPage from "./Products/ProductsPage";
+
+const Routes: React.FC = (props) => {
+  return (
+    <Switch>
+      <Route component={ItemsList} path="/" exact />
+      <Route component={ProductsPage} path="/products"  />
+    </Switch>
+  )
+}
+
+export default Routes;
