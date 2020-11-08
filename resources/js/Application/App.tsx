7bfdb1b15a6a71744b31ba2a6900d9043320a58e
Vitaly <vita-dubov@yandex.ru>
2020-11-08 18:59:35 +0400

    refactoring

--- resources/js/Application/App.tsx
+++ resources/js/Application/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import NavApp from "./components/Main/NavApp";
+import Index from "./components/Main/Index";
+import NavBar from "./components/NavBar/NavBar";
 import Routes from "./components/Routes";
 
 const App: React.FC = () => {
-  return (
-    <>
-      <NavApp />
-    </>
-  );
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawer = () => {
+        let status = !open;
+        setOpen(status);
+    };
+
+    return (
+        <>
+            <NavBar open={open} handleDrawer={handleDrawer} />
+            <Index open={open}>
+                <Routes />
+            </Index>
+        </>
+    );
 };
 
 export default App;
